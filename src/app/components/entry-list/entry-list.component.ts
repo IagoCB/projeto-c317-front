ce2bf6ce32b7eebfc702c8500cff7e46635fbbd4
Iago <iago.luiz@ges.inatel.br>
2023-04-24 20:16:13 -0300

    :lipstick: feat: Fixing bugs and adding dashboard

--- src/app/components/entry-list/entry-list.component.ts
+++ src/app/components/entry-list/entry-list.component.ts
@@ -88,6 +88,46 @@ export class EntryListComponent {
       classification: "Basic expanses",
       description: "Rent for the house",
     },
+    {
+      id: 1,
+      name: "Eletric bill",
+      date: this.handleDate(new Date()),
+      value: 125.5,
+      classification: "Basic expanses",
+      description: "Just a eletric bill",
+    },
+    {
+      id: 2,
+      name: "Water bill",
+      date: this.handleDate(new Date()),
+      value: 77.6,
+      classification: "Basic expanses",
+      description: "Just a water bill",
+    },
+    {
+      id: 3,
+      name: "GTX 4080",
+      date: this.handleDate(new Date()),
+      value: 8799,
+      classification: "Leisure expenses",
+      description: "Graphic cart for computer",
+    },
+    {
+      id: 4,
+      name: "Mouse razer",
+      date: this.handleDate(new Date()),
+      value: 350,
+      classification: "Leisure expenses",
+      description: "",
+    },
+    {
+      id: 5,
+      name: "Rent",
+      date: this.handleDate(new Date()),
+      value: 1456.7,
+      classification: "Basic expanses",
+      description: "Rent for the house",
+    },
   ];
   displayedColumns = ["name", "date", "value", "classification", "description", "action"];
 
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,8 +1,28 @@
 import { Component } from "@angular/core";
+import { Info } from "src/app/utils/model/info.model";
 
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard.component.scss"],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  infos: Info[] = [
+    {
+      name: "teste 1",
+      content: "teste content 1",
+    },
+    {
+      name: "teste 2",
+      content: "teste content 2",
+    },
+    {
+      name: "teste 3",
+      content: "teste content 3",
+    },
+    {
+      name: "teste 4",
+      content: "teste content 4",
+    },
+  ];
+}
